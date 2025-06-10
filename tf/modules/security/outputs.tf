c300f839c8cbc888b640b2fb5e4a662f1f347b22
wtluong <william.tl.luong@gmail.com>
2025-06-10 16:32:43 -0700

    added security group id

--- tf/modules/security/outputs.tf
+++ tf/modules/security/outputs.tf
@@ -3,3 +3,9 @@ output "bastion_security_group_id" {
   description = "ID of the bastion host security group"
   value       = aws_security_group.bastion.id
 }
+
+# Output the monitoring security group ID
+output "monitoring_security_group_id" {
+  description = "ID of the monitoring security group"
+  value       = aws_security_group.monitoring.id
+}
